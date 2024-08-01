81a06ff4d8b688767131c44129b1563c051a5a29
Anjali Krishna <anjalikrishna58082@gmail.com>
2024-08-01 11:19:20 +0530

    requirements.txt added

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from pydub.playback import play
 from io import BytesIO
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
